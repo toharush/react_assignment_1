87c4d8c017dd1091433e4c06f2cec5b4afbe954f
Toharush <toahr1010@gmail.com>
2022-11-25 16:16:49 +0200

    Added first assignment

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -8,13 +8,10 @@ function ShoppingList() {
     const [currentActiveItem, setCurrentActiveItem] = useState(undefined);
 
     return <div className="shoppingListBody">
-        { 
-            currentActiveItem ?
-            <ShoppingListItem name={currentActiveItem.name} image_path={currentActiveItem.image_path} price={currentActiveItem.price}
-            handleClick={() => setCurrentActiveItem(undefined)}
-            isCollapse={false} new_image_path={currentActiveItem.new_image_path} description={currentActiveItem.description} store_name={currentActiveItem.store_name}/>
-            :
-            data.map(item => <ShoppingListItem handleClick={() => setCurrentActiveItem(item)} name={item.name} image_path={item.image_path} price={item.price} isCollapse={true}/>)
+        {
+            currentActiveItem ? <ShoppingListItem name={currentActiveItem.name} image_path={currentActiveItem.image_path} price={currentActiveItem.price} handleClick={() => setCurrentActiveItem(undefined)}
+                isCollapse={false} new_image_path={currentActiveItem.new_image_path} description={currentActiveItem.description} store_name={currentActiveItem.store_name} /> :
+                data.map(item => <ShoppingListItem handleClick={() => setCurrentActiveItem(item)} name={item.name} image_path={item.image_path} price={item.price} isCollapse={true} />)
         }
     </div>
 };
